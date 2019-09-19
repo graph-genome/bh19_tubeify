883c4f2816bb38f1bc43720a2d21325e3deea12b
Josiah Seaman <josiah@newline.us>
2019-09-19 10:55:28 +0100

    Added Tileify stub function and a new test.json.

--- cli.js
+++ cli.js
@@ -4,10 +4,10 @@
  * Module dependencies.
  */
 
-const program = require('commander')
-const tubeify = require('./dist/index.js')
-const fs = require('fs')
-const parse = require('parse-jsonp')
+const program = require('commander');
+const tubeify = require('./dist/index.js');
+const fs = require('fs');
+const parse = require('parse-jsonp');
 
 // This is a testing method.
 /*
@@ -30,30 +30,31 @@ program
     .version('0.0.1', '-v, --version')
     .parse(process.argv);
 
-function readLines(input, func, callback) {
+function readLines(input, interpret_func, callback) {
     var remaining = '';
-    
-    input.on('data', function(data) {
+
+    input.on('data', function (data) {
         remaining += data;
+        remaining = remaining.replace('\r', '');
         var index = remaining.indexOf('\n');
-        var last  = 0;
+        var last = 0;
         while (index > -1) {
-        var line = remaining.substring(last, index);
-        last = index + 1;
-        func(line);
-        index = remaining.indexOf('\n', last);
+            var line = remaining.substring(last, index);
+            last = index + 1;
+            interpret_func(line);
+            index = remaining.indexOf('\n', last);
         }
-    
+
         remaining = remaining.substring(last);
     });
-    
-    input.on('end', function() {
+
+    input.on('end', function () {
         if (remaining.length > 0) {
-        func(remaining);
+            interpret_func(remaining);
         }
         callback()
     });
-    }
+}
 
 // console.log(program)
 // If an input file is JSON.
@@ -61,26 +62,22 @@ function readLines(input, func, callback) {
 
 // If an input file is JSONP.
 global.data = []
-function func(line) {
-    global.data.push(JSON.parse(line))
+
+function interpreter(line) {
+    let chunk = JSON.parse(line);
+    global.data.push(chunk)
 }
 
-function callback() {
-    let data = global.data
-    let input = []
-    data.forEach(item => {
-        item.bin_id = parseInt(item.bin_id) + 1
-        // item.begins = JSON.parse(item["begins"])
-        // item.ends = JSON.parse(item["ends"])
-    });
-    let tube = new tubeify.Tubeify(parseInt(program.tiles), parseInt(program.bin_length),  parseInt(program.bins));
-    // let tube = new tubeify.Tubeify(30, 300, 1000); For given 300 bins, we would like to have 30 tiles.
-    let json = tube.tubeify(data);
-    console.log(JSON.stringify(json))    
+function convertToTile() {
+    let data = global.data;
+    let converter = new tubeify.Tubeify(parseInt(program.tiles), parseInt(program.bin_length), parseInt(program.bins));
+    let json = converter.tileify(data);
+    console.log(JSON.stringify(json))
+
 }
 
 var input_data = fs.createReadStream(program.json);
-readLines(input_data, func, callback);
+readLines(input_data, interpreter, convertToTile);
 
 /*
 
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-interface Bin {
-    path_name: string,
-
-}
-
 export class Tubeify {
     private tile: number; // # of tiles in output. One tile corresponds to one bin if tiles === -1.
     private max_bin: number; // # of bins in input.
@@ -31,6 +26,18 @@ export class Tubeify {
         return tile_index
     }
 
+    tileify(bin_json: any){
+
+        let tubemap_json = {};
+        const nodes = Array.from(new Array(this.max_bin)).map((v, i) => { return { name: String(i + 1), sequenceLength: this.bin_length } }
+        );
+        tubemap_json["nodes"] = nodes;
+        tubemap_json["tracks"] = [{ id: 0, name: "REF", sequence: nodes.map(node => node.name) }];
+        tubemap_json["reads"] = reads;
+
+        return tubemap_json
+    }
+
     tubeify(bin_json: any) {
         let reads = [];
         let paths = {};
@@ -53,65 +60,6 @@ export class Tubeify {
         if (this.max_bin === -1) {
             this.max_bin = max_bin_id;
         }
-        /*
-        Object.keys(paths).forEach(path_name => {
-            let path_hash = paths[path_name];
-            let current_tile = -1;
-            let sequential_id = 0;
-            let tmp = [];
-            let current_pos = path_starts[path_name];
-
-            while(current_pos !== -1) {
-                let bin = path_hash[current_pos];
-                if (current_tile !== tiles(bin.bin)) {
-                    let sequence_new = tmp.map(a => a.);
-                    
-                    // Output as a single bin.
-                    let firstNodeOffset = tmp[0].bin - this.tiles_range[current_tile]; 
-                    if (firstNodeOffset > 0) {
-                        sequence_new.push({type: "link", pos: firstNodeOffset, seq: "translocation", query: bin.bin });
-                    }
-                    if (bin.bin - 1 !== tmp[-1].bin ) {
-                        sequence_new.push({type: "link", pos:(tmp.length) * this.bin_length, seq: "translocation", query: bin.bin });
-                    }
-
-                    reads.push({
-                        firstNodeOffset: firstNodeOffset * this.bin_length,
-                        finalNodeCoverLength: (tmp.length) * this.bin_length,
-                        mapping_quality: 60,
-                        is_secondary: false,
-                        Sequence: tmp.map(item => String(item.bin_id)),
-                        Sequence_new: sequence_new,
-                        type: "read",
-                        name: bin.path_name,
-                        id: sequential_id
-                   });
-
-                    sequential_id += 1;
-                    current_tile = tiles(bin.bin);
-
-                    tmp = [bin];
-                } else {
-                    tmp.push(bin);
-                }
-                bin.
-                current_pos = bin.next_bin[0];
-            }
-            // Clean up bins
-
-            reads.push({
-                firstNodeOffset: firstNodeOffset * this.bin_length,
-                finalNodeCoverLength: (tmp.length) * this.bin_length,
-                mapping_quality: 60,
-                is_secondary: false,
-                Sequence: tmp.map(item => item.bin),
-                Sequence_new: sequence_new,
-                type: "read",
-                name: bin.path_name,
-                id: sequential_id
-            });
-        })
-        */
 
 
         let previous_id = -1;
@@ -155,13 +103,13 @@ export class Tubeify {
                     id: path_unique_id,
                 });
             })
-        })
+        });
 
         // Merging nodes:
         let previous_read_id = -1;
         let previous_path_unique_id = -1;
         let previous_reads = [];
-        let new_reads = []
+        let new_reads = [];
         reads.forEach(read => {
             if ((read.id !== previous_path_unique_id || parseInt(read.sequence[0]) !== previous_read_id + 1) && previous_reads.length > 0) {
                 new_reads.push({
